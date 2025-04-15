6c3941ccfcb4a6f2c2c29a03b80c8299f46ecf44
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-04-15 22:50:51 +0530

    Add user check and page title

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import PaymentPage from '@/components/PaymentPage';
+import { notFound } from 'next/navigation';
+import connectDb from '@/db/connectDb';
+import User from '@/models/User';
 
-const Username = async ({params}) => {
-  const awaitedParams = await params;
-  console.log('params', awaitedParams);
+const Username = async ({ params }) => {
+  await connectDb();
+  const user = await User.findOne({ username: params.username });
+
+  if (!user) {
+    return notFound();
+  }
+
   return (
     <>
-      <PaymentPage username={awaitedParams.username} />
+      <PaymentPage username={params.username} />
     </>
   );
 };
 
 export default Username;
+
+export async function generateMetadata({ params }) {
+  return {
+    title: `Support ${params.username} - Get Me A Chai`,
+  }
+}
+
