eb5f7bb0fdd87144afacdf5ec835a7679dbc25b8
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-04-16 13:42:29 +0530

    Update Razorpay payment verification logic

--- app/api/razorpay/route.js
+++ app/api/razorpay/route.js
@@ -3,6 +3,7 @@ import {validatePaymentVerification} from "razorpay/dist/utils/razorpay-utils";
 import Payment from "@/models/Payment";
 import Razorpay from "razorpay";
 import connectDb from "@/db/connectDb";
+import User from "@/models/User";
 
 export const POST = async (req) => {
   await connectDb()
@@ -14,10 +15,13 @@ export const POST = async (req) => {
     return NextResponse.json({success: false, message:"Order Id not found"})
   }
 
-  let xx = validatePaymentVerification({"order_id": body.razorpay_order_id, "payment_id": body.razorpay_payment_id}, body.razorpay_signature, process.env.NEXT_PUBLIC_KEY_SECRET)
+  let userData = await User.findOne({username: p.to_user})
+  const secret = userData.razorpaysecret
+
+  let xx = validatePaymentVerification({"order_id": body.razorpay_order_id, "payment_id": body.razorpay_payment_id}, body.razorpay_signature, secret)
 
   if (xx) {
-    const updatePayment = await Payment.findOneAndUpdate({oid: body.razorpay_order_id}, {done: "true"}, {new: true})
+    const updatePayment = await Payment.findOneAndUpdate({oid: body.razorpay_order_id}, {done: true}, {new: true})
     return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/${updatePayment.to_user}?paymentdone=true`)
   }
 
