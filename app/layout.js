8d256d5fcda98542ff9bf66dc68edb4ce8aeac66
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-04-16 13:56:21 +0530

    Refactor layout and adjust background styles

--- app/layout.js
+++ app/layout.js
@@ -1,36 +1,37 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import SessionWrapper from "@/components/SessionWrapper";
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import SessionWrapper from '@/components/SessionWrapper';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata = {
-  title: "Get me A Chai - Fund your projects with chai ",
-  description: "This website is a crowdfunding platform for creators.",
+  title: 'Get me A Chai - Fund your projects with chai ',
+  description: 'This website is a crowdfunding platform for creators.',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-      <SessionWrapper>
-        <Navbar/>
-        <div className="min-h-[83vh] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
-        {children}
-        </div>
-        <Footer/>
-      </SessionWrapper>
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
+      >
+        <SessionWrapper>
+          <Navbar />
+          <div className="flex-grow bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white">
+            {children}
+          </div>
+          <Footer />
+        </SessionWrapper>
       </body>
     </html>
   );
